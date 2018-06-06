a07590d2c65430cb412d3ef8ad26d23c5d2eb1d1
bagubragowo <38901345+bagubragowo@users.noreply.github.com>
2018-06-06 11:52:45 +0700

    Update header.php

--- DSS/header.php
+++ DSS/header.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
   <div class="container">
   <a class="navbar-brand" href="index.php">SAW</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
